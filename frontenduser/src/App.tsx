2c2e2686345736860b3d83b6108796c82421752f
blazejzj <opexrrator@gmail.com>
2025-04-20 15:51:35 +0200

    feat: initial commit to frontenduser vite/ts/tailwind

---
+++ frontenduser/src/App.tsx
@@ -0,0 +1,12 @@
+import "../index.css";
+function App() {
+    return (
+        <div className="container">
+            <h1 className="text-5xl">
+                This is my first text in TSX + Tailwind
+            </h1>
+        </div>
+    );
+}
+
+export default App;
