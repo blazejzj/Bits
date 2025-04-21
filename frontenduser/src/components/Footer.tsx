e1537722df323e5663619626316e4bc70224062a
blazejzj <opexrrator@gmail.com>
2025-04-21 19:07:58 +0200

    feat: added more lorem into footer to be more full

--- frontenduser/src/components/Footer.tsx
+++ frontenduser/src/components/Footer.tsx
@@ -5,7 +5,7 @@ import { faGithub } from "@fortawesome/free-brands-svg-icons";
 function Footer() {
     function renderFooter() {
         return (
-            <div className="bg-white flex flex-row justify-around mt-3 p-5">
+            <div className="bg-white flex flex-row justify-between mt-3 p-5 items-center">
                 <div>
                     <NavLink to="/">
                         <p className="font-bold text-5xl text-cyan-700 ml-5 mr-3 flex items-baseline flex-nowrap text-nowrap">
@@ -13,6 +13,7 @@ function Footer() {
                         </p>
                     </NavLink>
                 </div>
+                <p>Copyright 2025</p>
                 <div className="flex flex-row items-center justify-center gap-2">
                     <p>Made by opex</p>
                     <a
