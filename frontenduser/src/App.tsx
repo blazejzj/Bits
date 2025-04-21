d90f5bc4f68588f9248b7b096932ab8ec1f263a3
blazejzj <opexrrator@gmail.com>
2025-04-21 14:27:46 +0200

    feat: header has been styled

--- frontenduser/src/App.tsx
+++ frontenduser/src/App.tsx
@@ -4,7 +4,7 @@ import Header from "./components/Header";
 
 function App() {
     return (
-        <div className="container">
+        <div className="app w-full">
             <Header />
             <main>
                 <Outlet />
--- frontenduser/src/components/Header.tsx
+++ frontenduser/src/components/Header.tsx
@@ -26,14 +26,9 @@ function Header() {
                 }
 
                 const data = await response.json();
-                console.log(data);
                 setCategories(data);
             } catch (error) {
-                if (error instanceof Error) {
-                    console.error(error.message);
-                } else {
-                    console.error("Unknown error:", error);
-                }
+                console.error(error);
             }
         }
         getCategories();
@@ -44,33 +39,87 @@ function Header() {
         navigate("/");
     }
 
+    const linkBaseClasses =
+        "text-xl font-medium gradient‑wipe transition duration-500 ease-in-out border-b-2";
+
     function renderLoggedIn() {
         return (
             <>
-                <NavLink to="/profile">Profile</NavLink>
-                <button onClick={handleLogout}>Log out</button>
+                <NavLink
+                    to="/profile"
+                    className={({ isActive }) =>
+                        `${linkBaseClasses} ${
+                            isActive ? "border-cyan-700" : "border-transparent"
+                        }`
+                    }
+                >
+                    Profile
+                </NavLink>
+                <button
+                    onClick={handleLogout}
+                    className="text-xl font-medium ml-5"
+                >
+                    Log out
+                </button>
             </>
         );
     }
 
     function renderLoggedOut() {
         return (
-            <>
-                <NavLink to="/login">Log in</NavLink>
-            </>
+            <NavLink
+                to="/login"
+                className={({ isActive }) =>
+                    `${linkBaseClasses} ${
+                        isActive ? "border-cyan-700" : "border-transparent"
+                    }`
+                }
+            >
+                Log in
+            </NavLink>
         );
     }
 
     return (
-        <div>
-            <h1>Bits</h1>
-            <nav>
-                <ul>
-                    {categories.map((category) => {
-                        return <li key={category.id}>{category.name}</li>;
-                    })}
+        <div className="flex flex-row w-full justify-between p-5 sticky shadow">
+            <NavLink to="/">
+                <h1 className="font-bold text-5xl text-cyan-700 ml-5">
+                    Bits <b className="text-xs">by opex</b>
+                </h1>
+            </NavLink>
+            <nav className="flex flex-row w-3/4 items-center">
+                <ul className="flex gap-5 w-full content-center justify-center">
+                    <NavLink
+                        to="/"
+                        className={({ isActive }) =>
+                            `${linkBaseClasses} ${
+                                isActive
+                                    ? "border-cyan-700"
+                                    : "border-transparent"
+                            }`
+                        }
+                    >
+                        Home
+                    </NavLink>
+
+                    {categories.map((category) => (
+                        <NavLink
+                            key={category.id}
+                            to={`/posts?category=${category.id}`}
+                            className={({ isActive }) =>
+                                `${linkBaseClasses} ${
+                                    isActive
+                                        ? "border-cyan-700"
+                                        : "border-transparent"
+                                }`
+                            }
+                        >
+                            {category.name}
+                        </NavLink>
+                    ))}
+
+                    {user ? renderLoggedIn() : renderLoggedOut()}
                 </ul>
-                {user ? renderLoggedIn() : renderLoggedOut()}
             </nav>
         </div>
     );
