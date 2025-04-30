f95de547d6bc078ca9ed42122ff34089d72fc847
blazejzj <opexrrator@gmail.com>
2025-04-30 18:57:06 +0200

    feat: post fetching now works, can fetched based on categories

--- frontenduser/src/components/Posts.tsx
+++ frontenduser/src/components/Posts.tsx
@@ -1,16 +1,71 @@
-// import { useEffect, useState } from "react";
-// import { useSearchParams } from "react-router-dom";
-// import { Post } from "../types/post";
+import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
+import { Post } from "../types/post";
 // import { slugify } from "../utils/slugify";
 
 function Posts() {
-    // const [posts, setPosts] = useState<Post[]>();
+    const [posts, setPosts] = useState<Post[]>([]);
+    const [searchParams, setSearchParams] = useSearchParams();
 
-    // useEffect(() => {});
-    // const [searchParams, setSearchParams] = useSearchParams();
+    // postsRouter.get(
+    //     "/category/:slugname",
+    //     postsController.getPostsByCategorySlugname
+    // );
 
-    // console.log(searchParams);
-    return <p>hey</p>;
+    useEffect(() => {
+        async function getPosts() {
+            if (searchParams.size > 0) {
+                const category = searchParams.get("category");
+                try {
+                    const response = await fetch(
+                        `${
+                            import.meta.env.VITE_API_URL
+                        }/posts/category/${category}`
+                    );
+                    if (!response.ok) {
+                        console.log(await response.json());
+                    } else {
+                        const data = await response.json();
+                        setPosts(data);
+                    }
+                } catch (err) {
+                    if (err instanceof Error) {
+                        console.log(err.message);
+                    }
+                }
+            } else {
+                try {
+                    const response = await fetch(
+                        `${import.meta.env.VITE_API_URL}/posts`
+                    );
+                    if (!response.ok) {
+                        console.log(await response.json());
+                    } else {
+                        const data = await response.json();
+                        setPosts(data);
+                    }
+                } catch (err) {
+                    if (err instanceof Error) {
+                        console.log(err.message);
+                    }
+                }
+            }
+        }
+        getPosts();
+    }, [searchParams]);
+
+    return (
+        <div>
+            <h1>Here are your posts</h1>
+            <p>Category chosen {searchParams.get("category")}</p>
+            {posts.map((post) => {
+                return (
+                    <div key={post.id}>
+                        <p>{post.title}</p>
+                    </div>
+                );
+            })}
+        </div>
+    );
 }
-
 export default Posts;
