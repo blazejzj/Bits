094bb42ab21ffcc1925ae825ed786aae53aa7f46
blazejzj <opexrrator@gmail.com>
2025-05-09 21:41:39 +0200

    feat: admin main page styled (root) + header

--- frontendadmin/src/App.tsx
+++ frontendadmin/src/App.tsx
@@ -8,17 +8,20 @@ function App() {
     const { user } = useAuth();
 
     return (
-        <div>
-            {user && user.role == "ADMIN" ? (
-                <div>
-                    <p>{user.name} hehe</p>
-                    <Header />
-                    <Outlet />
-                    <Footer />
-                </div>
-            ) : (
-                <LogInPage />
-            )}
+        <div className="min-h-screen bg-gray-200 flex flex-col">
+            <div className="container mx-auto px-4 flex-1 flex flex-col">
+                {user && user.role == "ADMIN" ? (
+                    <div className="flex flex-col h-lvh">
+                        <Header />
+                        <main className="bg-white flex-1 p-6 shadow-sm flex h-max w-full">
+                            <Outlet />
+                        </main>
+                        <Footer />
+                    </div>
+                ) : (
+                    <LogInPage />
+                )}
+            </div>
         </div>
     );
 }
--- frontendadmin/src/components/Header.tsx
+++ frontendadmin/src/components/Header.tsx
@@ -1,19 +1,72 @@
+import { NavLink, useNavigate } from "react-router";
+import useAuth from "../hooks/useAuth";
 import Logo from "./Logo";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser, faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
 
 function Header() {
+    const { logout } = useAuth();
+    const navigate = useNavigate();
+
+    function handleLogout() {
+        logout();
+        navigate("/");
+    }
+
+    const btnBase =
+        "flex items-center px-4 py-1 rounded-full transition cursor-pointer ";
+    const active = "bg-gray-700 text-white hover:bg-gray-600";
+    const navLinkStyles = {
+        base: "text-base font-medium transition-[background-position] duration-500 whitespace-nowrap px-3 py-1 rounded-full cursor-pointer",
+        active: "bg-cyan-600 text-white hover:bg-cyan-700",
+        inactive: "gradient-wipe text-black",
+    };
     function renderMenu() {
         return (
             <nav>
-                <ul>
-                    <li>wow1</li>
-                    <li>wow2</li>
-                    <li>wow3</li>
+                <ul className="flex gap-16">
+                    <li>
+                        <NavLink
+                            to={`/manage`}
+                            className={({ isActive }) =>
+                                [
+                                    navLinkStyles.base,
+                                    isActive
+                                        ? navLinkStyles.active
+                                        : navLinkStyles.inactive,
+                                ].join(" ")
+                            }
+                        >
+                            Panel
+                        </NavLink>
+                    </li>
+                    <div className="flex gap-4">
+                        <NavLink
+                            to="/profile"
+                            className={btnBase + active}
+                            data-testid="profile-button"
+                        >
+                            <FontAwesomeIcon icon={faUser} className="mr-2" />
+                            Profile
+                        </NavLink>
+                        <button
+                            onClick={handleLogout}
+                            className={btnBase + active}
+                            data-testid="logout-button"
+                        >
+                            <FontAwesomeIcon
+                                icon={faSignOutAlt}
+                                className="mr-2"
+                            />
+                            Log out
+                        </button>
+                    </div>
                 </ul>
             </nav>
         );
     }
     return (
-        <div>
+        <div className="flex justify-between items-center p-6">
             <Logo />
             {renderMenu()}
         </div>
