8a19d9864b690f3425974ec4b297b0da6209cb5e
blazejzj <opexrrator@gmail.com>
2025-05-15 16:05:24 +0200

    feat: updating name now works, errors and message displays nicely. have to update name dynamically in profile now

--- frontendadmin/src/components/profile/ProfileNameForm.tsx
+++ frontendadmin/src/components/profile/ProfileNameForm.tsx
@@ -1,4 +1,4 @@
-import { useState, type ChangeEvent } from "react";
+import { useState, type ChangeEvent, type FormEvent } from "react";
 
 interface ProfileNameFormProps {
     toggleNameChange: () => void;
@@ -6,31 +6,115 @@ interface ProfileNameFormProps {
 
 function ProfileNameForm({ toggleNameChange }: ProfileNameFormProps) {
     const [formData, setFormData] = useState({
-        username: "",
+        name: "",
         authPassword: "",
     });
+    const [error, setError] = useState<string>("");
+    const [message, setMessage] = useState<string>("");
 
     function handleInputChange(e: ChangeEvent<HTMLInputElement>) {
         setFormData((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+        setError("");
+        setMessage("");
     }
 
-    async function handleFormSubmit() {}
+    function resetFormData() {
+        setFormData({
+            name: "",
+            authPassword: "",
+        });
+    }
+
+    async function handleFormSubmit(e: FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+        try {
+            const response = await fetch(
+                `${import.meta.env.VITE_API_URL}/profile`,
+                {
+                    method: "PATCH",
+                    credentials: "include",
+                    headers: {
+                        "Content-type": "application/json",
+                    },
+                    body: JSON.stringify(formData),
+                }
+            );
+            if (!response.ok) {
+                const body = await response.json();
+                if (Array.isArray(body.msg)) {
+                    setError(body.msg[0].msg);
+                } else {
+                    setError(body.msg);
+                }
+            } else {
+                console.log("else2");
+                const body = await response.json();
+                console.log(body.msg);
+                setMessage(body.msg);
+            }
+        } catch (err) {
+            if (err instanceof Error) {
+                setError("Internal server issues. Couldn't update value.");
+            }
+        }
+        resetFormData();
+    }
 
     function renderForm() {
         return (
-            <form onSubmit={handleFormSubmit}>
-                <label htmlFor="newName">New Name</label>
-                <input type="text" name="newName" id="newName" />
-                <label htmlFor="authPassword">Password</label>
-                <input
-                    type="authPassword"
-                    name="authPassword"
-                    id="authPassword"
-                />
-                <button type="submit">Change Name</button>
-                <button onClick={toggleNameChange} className="cursor-pointer">
-                    Cancel
-                </button>
+            <form
+                onSubmit={handleFormSubmit}
+                className="ml-3 flex flex-col gap-3"
+            >
+                {error && (
+                    <p className="bg-red-100 text-red-700 border border-red-200 rounded-md px-4 py-2">
+                        {error}
+                    </p>
+                )}
+                {message && (
+                    <p className="bg-green-100 text-green-700 border border-green-200 rounded-md px-4 py-2">
+                        {message}
+                    </p>
+                )}
+                <div className="flex flex-col gap-2">
+                    <label htmlFor="name" className="font-bold text-l">
+                        New Name
+                    </label>
+                    <input
+                        type="text"
+                        name="name"
+                        id="name"
+                        required
+                        value={formData.name}
+                        onChange={handleInputChange}
+                        className="border-gray-200 border px-3 py-2 rounded-md focus:border-gray-600 focus:outline-none focus:ring-2"
+                    />
+                </div>
+                <div className="flex flex-col gap-2">
+                    <label htmlFor="authPassword" className="font-bold text-l">
+                        Password
+                    </label>
+                    <input
+                        type="password"
+                        name="authPassword"
+                        id="authPassword"
+                        required
+                        value={formData.authPassword}
+                        onChange={handleInputChange}
+                        className="border-gray-200 border px-3 py-2 rounded-md focus:border-gray-600 focus:outline-none focus:ring-2"
+                    />
+                </div>
+                <div className="flex gap-4 mt-2">
+                    <button type="submit" className="cursor-pointer font-bold">
+                        Update
+                    </button>
+                    <button
+                        onClick={toggleNameChange}
+                        className="cursor-pointer font-bold"
+                    >
+                        Cancel
+                    </button>
+                </div>
             </form>
         );
     }
