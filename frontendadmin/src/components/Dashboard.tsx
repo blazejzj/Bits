3c7abef1aa7b2df17c7565fb2b185dd6316e564c
blazejzj <opexrrator@gmail.com>
2025-05-15 19:00:09 +0200

    feat: dashboard layout styled, not blank and boring anymore, now onto the main functionaltiy

--- frontendadmin/src/components/Dashboard.tsx
+++ frontendadmin/src/components/Dashboard.tsx
@@ -1,5 +1,24 @@
+import useAuth from "../hooks/useAuth";
+import developer from "../assets/developer.svg";
+
 function Dashboard() {
-    return <p>Hello from dashboard</p>;
+    const { user } = useAuth();
+    function renderGreeting() {
+        return (
+            <h1 className="text-5xl">
+                Welcome{" "}
+                <span className="font-bold text-gray-600">{user!.name}</span>{" "}
+                how are you today?
+            </h1>
+        );
+    }
+
+    return (
+        <div className="flex flex-col gap-7 items-center justify-center w-full from-left">
+            {renderGreeting()}
+            <img src={developer} alt="Dev" />
+        </div>
+    );
 }
 
 export default Dashboard;
