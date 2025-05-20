67e9bde1ae7e739f86f47216273e7e7251359fa8
blazejzj <opexrrator@gmail.com>
2025-05-20 16:57:40 +0200

    feat: creating categories now works

--- frontendadmin/src/components/managing/ManageCategories.tsx
+++ frontendadmin/src/components/managing/ManageCategories.tsx
@@ -3,6 +3,7 @@ import type { Category } from "../../types/category";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash, faPenToSquare } from "@fortawesome/free-solid-svg-icons";
 import { slugify } from "../../utils/slugify";
+import ErrorMessage from "../../utils/ErrorMessage";
 
 function ManageCategories() {
     const [categories, setCategories] = useState<Category[]>([]);
@@ -11,28 +12,30 @@ function ManageCategories() {
     );
     const [oldCategoryName, setOldCategoryName] = useState<string>("");
     const [editedCategoryName, setEditedCategoryName] = useState<string>("");
+    const [newCategoryName, setNewCategoryName] = useState<string>("");
+    const [createCategoryMessage, setCreateCategoryMessage] =
+        useState<string>("");
 
-    useEffect(() => {
-        async function fetchCategories() {
-            try {
-                const response = await fetch(
-                    `${import.meta.env.VITE_API_URL}/posts/category`,
-                    {
-                        method: "GET",
-                        credentials: "include",
-                    }
-                );
-                if (response.ok) {
-                    const body = await response.json();
-                    setCategories(body);
+    async function fetchCategories() {
+        try {
+            const response = await fetch(
+                `${import.meta.env.VITE_API_URL}/posts/category`,
+                {
+                    method: "GET",
+                    credentials: "include",
                 }
-            } catch (err) {
-                if (err instanceof Error) {
-                    console.error(err);
-                }
+            );
+            if (response.ok) {
+                const body = await response.json();
+                setCategories(body);
+            }
+        } catch (err) {
+            if (err instanceof Error) {
+                console.error(err);
             }
         }
-
+    }
+    useEffect(() => {
         fetchCategories();
     }, []);
 
@@ -40,9 +43,12 @@ function ManageCategories() {
         setOldCategoryName("");
         setEditedCategoryName("");
         setEditedCategoryId(null);
+        setNewCategoryName("");
     }
 
-    async function deleteCategory(id: number) {}
+    async function deleteCategory(id: number) {
+        console.log(id);
+    }
 
     async function updateCategory(e: FormEvent<HTMLFormElement>) {
         e.preventDefault();
@@ -87,7 +93,7 @@ function ManageCategories() {
 
         try {
             // /category/:slugname/updateslugname
-            const response = await fetch(
+            await fetch(
                 `${import.meta.env.VITE_API_URL}/posts/category/${
                     updateDataSlug.slugname
                 }/updateslugname`,
@@ -98,18 +104,39 @@ function ManageCategories() {
                     body: JSON.stringify(updateDataSlug),
                 }
             );
-            if (!response.ok) {
-                const body = await response.json();
-                console.log(body);
-            } else {
-                resetFormData();
-            }
         } catch (err) {
             console.log(err);
         }
     }
 
-    async function createCategory(name: string) {}
+    async function createCategory(e: FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+        const data = {
+            name: newCategoryName,
+            slugname: slugify(newCategoryName),
+        };
+
+        try {
+            const response = await fetch(
+                `${import.meta.env.VITE_API_URL}/posts/category/new`,
+                {
+                    method: "POST",
+                    credentials: "include",
+                    body: JSON.stringify(data),
+                    headers: { "Content-type": "application/json" },
+                }
+            );
+            if (!response.ok) {
+                const body = await response.json();
+                setCreateCategoryMessage(body.msg);
+            } else {
+                resetFormData();
+                fetchCategories();
+            }
+        } catch (err) {
+            console.error(err);
+        }
+    }
 
     function toggleEditCategory(id: number, name: string) {
         if (editedCategoryId === null) {
@@ -126,83 +153,138 @@ function ManageCategories() {
 
     function renderAllCategories() {
         return (
-            <div className="p-7 flex flex-col gap-4">
-                <h1 className="font-bold text-2xl">Categories:</h1>
-                {categories.map((cat) => (
-                    <div key={cat.id}>
-                        {editedCategoryId === cat.id ? (
-                            <form
-                                className="flex gap-2 flex-col"
-                                onSubmit={updateCategory}
-                            >
-                                <input
-                                    type="text"
-                                    value={editedCategoryName}
-                                    required
-                                    onChange={(e) =>
-                                        setEditedCategoryName(e.target.value)
-                                    }
-                                    className="border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-gray-700"
-                                />
-                                <div className="flex gap-2 mt-3">
-                                    <button
-                                        className="cursor-pointer text-white px-3 py-2 rounded-md hover:bg-gray-500 font-bold bg-gray-600"
-                                        type="submit"
-                                    >
-                                        Update
-                                    </button>
-                                    <button
-                                        onClick={() =>
-                                            toggleEditCategory(cat.id, cat.name)
+            <div className="h-full bg-white shadow rounded-lg p-6 flex flex-col">
+                <h2 className="text-2xl font-bold  mb-4 ml-1">
+                    All Categories
+                </h2>
+                <div className="flex flex-col">
+                    {categories.map((cat) => (
+                        <div
+                            key={cat.id}
+                            className="flex items-center justify-between py-2 border-b last:border-b-0 ml-1 border-gray-300"
+                        >
+                            {editedCategoryId === cat.id ? (
+                                <form
+                                    onSubmit={updateCategory}
+                                    className="w-full flex flex-col gap-2"
+                                >
+                                    <input
+                                        type="text"
+                                        value={editedCategoryName}
+                                        required
+                                        onChange={(e) =>
+                                            setEditedCategoryName(
+                                                e.target.value
+                                            )
                                         }
-                                        className="cursor-pointer text-gray-800 hover:text-gray-600 font-bold"
-                                    >
-                                        Cancel
-                                    </button>
-                                </div>
-                            </form>
-                        ) : (
-                            <div className="flex gap-2">
-                                <span>{cat.name}</span>
-                                <div className="flex gap-1">
-                                    <button
-                                        onClick={() => (
-                                            toggleEditCategory(
-                                                cat.id,
-                                                cat.name
-                                            ),
-                                            setOldCategoryName(cat.name)
-                                        )}
-                                        className="cursor-pointer text-gray-800 hover:text-gray-600"
-                                    >
-                                        <FontAwesomeIcon
-                                            icon={faPenToSquare}
-                                            size="lg"
-                                        />
-                                    </button>
-                                    <button
-                                        onClick={() => deleteCategory(cat.id)}
-                                        className="cursor-pointer text-gray-800 hover:text-gray-600"
-                                    >
-                                        <FontAwesomeIcon
-                                            icon={faTrash}
-                                            size="lg"
-                                        />
-                                    </button>
-                                </div>
-                            </div>
-                        )}
-                    </div>
-                ))}
+                                        className="w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-gray-500"
+                                    />
+                                    <div className="flex gap-2 mt-2">
+                                        <button
+                                            type="submit"
+                                            className="px-4 py-2 bg-gray-600 text-white rounded-md font-medium hover:bg-gray-500 cursor-pointer"
+                                        >
+                                            Update
+                                        </button>
+                                        <button
+                                            type="button"
+                                            onClick={() =>
+                                                toggleEditCategory(
+                                                    cat.id,
+                                                    cat.name
+                                                )
+                                            }
+                                            className="px-4 py-2 text-gray-600 rounded-md font-medium hover:bg-gray-100 cursor-pointer "
+                                        >
+                                            Cancel
+                                        </button>
+                                    </div>
+                                </form>
+                            ) : (
+                                <>
+                                    <span className="text-gray-800">
+                                        {cat.name}
+                                    </span>
+                                    <div className="flex gap-3">
+                                        <button
+                                            onClick={() => {
+                                                toggleEditCategory(
+                                                    cat.id,
+                                                    cat.name
+                                                );
+                                                setOldCategoryName(cat.name);
+                                            }}
+                                            className="p-2 cursor-pointer hover:text-gray-500 text-gray-700"
+                                        >
+                                            <FontAwesomeIcon
+                                                icon={faPenToSquare}
+                                                size="lg"
+                                            />
+                                        </button>
+                                        <button
+                                            onClick={() =>
+                                                deleteCategory(cat.id)
+                                            }
+                                            className="p-2 cursor-pointer hover:text-gray-500 text-gray-700"
+                                        >
+                                            <FontAwesomeIcon
+                                                icon={faTrash}
+                                                size="lg"
+                                            />
+                                        </button>
+                                    </div>
+                                </>
+                            )}
+                        </div>
+                    ))}
+                </div>
             </div>
         );
     }
 
+    function renderNewCategoryForm() {
+        return (
+            <form
+                onSubmit={createCategory}
+                className="h-full bg-white shadow rounded-lg p-6 flex flex-col"
+            >
+                <h2 className="text-2xl font-bold mb-4">Create New Category</h2>
+                <div className="mb-4 flex flex-col gap-3">
+                    {createCategoryMessage && (
+                        <ErrorMessage errorMsg={createCategoryMessage} />
+                    )}
+                    <label
+                        htmlFor="name"
+                        className="block text-gray-700 font-medium mb-1"
+                    >
+                        Category Name
+                    </label>
+                    <input
+                        type="text"
+                        name="name"
+                        id="name"
+                        required
+                        onChange={(e) => setNewCategoryName(e.target.value)}
+                        value={newCategoryName}
+                        placeholder="New category name..."
+                        className="w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-gray-500"
+                    />
+                    <button
+                        type="submit"
+                        className="mt-auto px-6 py-3 bg-gray-600 text-white rounded-md font-medium hover:bg-gray-500 self-start cursor-pointer"
+                    >
+                        Create
+                    </button>
+                </div>
+            </form>
+        );
+    }
+
     return (
-        <div className="flex gap-3 justify-center items-center w-full h-full ">
-            <div className="flex">
+        <div className="container mx-auto mt-10">
+            <div className="grid grid-cols-2 gap-6 h-[500px]">
                 {renderAllCategories()}
-                <h1>Form to add categories here</h1>
+                {renderNewCategoryForm()}
             </div>
         </div>
     );
